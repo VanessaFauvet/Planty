267e8bd06bca7b9a7071353fa6cbec0c5b01c500
Vanessa Fauvet <vanessa.fauvet75@gmail.com>
2024-01-04 00:04:59 +0100

    Modification du header

---
+++ wp-content/themes/blankslatechild/footer.php
@@ -0,0 +1,11 @@
+            </main>
+            <?php get_sidebar(); ?>
+        </div>
+
+        <footer id="footer" role="contentinfo">
+            <nav class="footer_nav"><?php wp_nav_menu(['theme_location' => 'footer']) ?></nav>
+        </footer>
+    </div>
+    <?php wp_footer(); ?>
+</body>
+</html>
